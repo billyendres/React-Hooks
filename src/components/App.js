7d8063fdebc01d39356ac363ab0806c3611810de
BILLY ENDRES <billyendres@BILLYs-MacBook-Pro.local>
2019-05-28 16:18:37 +1000

    State initialization

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { resource: "" };
+	}
+
+	render() {
+		return (
+			<div>
+				<div>
+					<button onClick={() => this.setState({ resource: "post" })}>Posts</button>
+					<button onClick={() => this.setState({ resource: "todos" })}>Todos</button>
+				</div>
+				{this.state.resource}
+			</div>
+		);
+	}
+}
+
+export default App;
